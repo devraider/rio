f21ba4d12911955acd25c011198f4794975b5cef
crtpaul <cristeapaul.2015@gmail.com>
2025-12-14 16:48:39 +0200

    feat: add buffered header functions

--- src/rio.h
+++ src/rio.h
@@ -18,4 +18,9 @@ typedef struct
 ssize_t rio_readn(int fd, void *usrbuf, size_t n);
 ssize_t rio_writen(int fd, void *usrbuf, size_t n);
 
+/* Buffered I/O functions */
+void rio_readinitb(rio_t *rp, int fd);
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen);
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n);
+
 #endif
