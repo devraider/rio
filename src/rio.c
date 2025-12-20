f48f735cbe2986a49e9b7c4ddf8ced8008d896bd
crtpaul <cristeapaul.2015@gmail.com>
2025-12-20 20:23:04 +0200

    feat: add rio_readnb

--- src/rio.c
+++ src/rio.c
@@ -57,3 +57,22 @@ void rio_readinitb(rio_t *rp, int fd)
     rp->rio_cnt = 0;
     rp->rio_bufptr = rp->rio_buf;
 }
+
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0)
+    {
+        if ((nread = rio_read(rp, bufp, nleft)) < 0)
+            // errno set by read()
+            return -1;
+        else if (nread == 0)
+            break;
+        nleft -= nread;
+        bufp += nread;
+    }
+    return (n - nleft);
+}
