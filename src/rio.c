2f936f886365cba483aee4f4ce010069eb81b593
crtpaul <cristeapaul.2015@gmail.com>
2025-12-20 20:27:00 +0200

    feat: add rio_readlineb

--- src/rio.c
+++ src/rio.c
@@ -76,3 +76,35 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
     }
     return (n - nleft);
 }
+
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen)
+{
+    int n, rc;
+    char c, *bufp = usrbuf;
+
+    for (n = 1; n < maxlen; n++)
+    {
+        // Read a single byte
+        if ((rc = rio_read(rp, &c, 1)) == 1)
+        {
+            *bufp++ = c;
+            if (c == '\n')
+            {
+                n++;
+                break;
+            }
+        }
+        // EOF
+        else if (rc == 0)
+        {
+            if (n == 1)
+                return 0;
+            else
+                break;
+        }
+        else
+            return -1;
+    }
+    *bufp = 0;
+    return n - 1;
+}
